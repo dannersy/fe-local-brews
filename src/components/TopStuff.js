d1f2573edfde8b4ad7c8806be95b53a1a3e3af86
Daniel <danielyochumjr@gmail.com>
2016-12-29 12:22:28 -0500

    thurs tweaks

--- src/components/TopStuff.js
+++ src/components/TopStuff.js
@@ -1,30 +1,28 @@
 import React, { Component } from 'react';
 import { Navbar, FormGroup, FormControl, Button } from 'react-bootstrap';
 
-const navbarInstance = (
-  <Navbar>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">Brand</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Navbar.Form pullLeft>
-        <FormGroup>
-          <FormControl type="text" placeholder="Search" />
-        </FormGroup>
-        {' '}
-        <Button type="submit">Submit</Button>
-      </Navbar.Form>
-    </Navbar.Collapse>
-  </Navbar>
-);
-
 class TopStuff extends Component {
   render(){
-    return navbarInstance
+    return (
+      <Navbar fluid>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <p>Local Brews</p>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Form pullLeft>
+            <FormGroup>
+              <FormControl type="text" placeholder="Search Zip" />
+            </FormGroup>
+            {' '}
+            <Button type="submit">Submit</Button>
+          </Navbar.Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
   }
-}
+};
 
 export default TopStuff
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 import TopStuff from "../components/TopStuff.js";
 import Breweries from "../components/Breweries.js";
 import Map from "../components/Map.js";
@@ -7,14 +8,14 @@ import '../stylesheets/App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <TopStuff />
-        <div>
-          <div>
-          <Breweries />
-          </div>
-          <Map />
-        </div>
+        <Grid>
+          <Row className="breweries">
+            <Col sm={6} md={6}><Map /></Col>
+            <Col sm={6} md={6}><Breweries /></Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
