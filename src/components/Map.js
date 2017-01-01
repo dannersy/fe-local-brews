1a569b20b18f4a66f55bce5a7a6a46689dbdca8e
Daniel <danielyochumjr@gmail.com>
2016-12-31 20:23:30 -0500

    marker is working

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 import GoogleMap from 'google-map-react';
+import Marker from "./Marker.js"
 
-export class Map extends Component {
+class Map extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      center: {lat: 39.50, lng: -98.35},
+      zoom: 4
+    }
+  }
+
+  _locations(){
+    // this.setState({
+    //   center: {
+    //     lat: this.props.breweries[0].lat,
+    //     lng: this.props.breweries[0].lng
+    //   },
+    //   zoom: 7
+    // })
+    // return console.log(this.state.center);
+  }
 
   render() {
     return (
@@ -10,10 +30,12 @@ export class Map extends Component {
           key: 'AIzaSyBwV_7tbM72Vm45JzB3CX-1nv9w7s14wh0',
           language: 'en'
         }}
-        defaultCenter={{lat: 59.938043, lng: 30.337157}}
-        defaultZoom={9}>
+        center={this.state.center}
+        zoom={this.state.zoom}>
+        <Marker lat={39.50} lng={-98.35} text={'A'} />
+        {this.props.breweries.length ? this._locations() : null}
       </GoogleMap>
-    );
+    )
   }
 }
 
