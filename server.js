8f64a3313968b7a3eac956caef8100c284f9d114
Daniel <danielyochumjr@gmail.com>
2017-01-08 17:22:20 -0500

    server stuff for deployment

---
+++ server.js
@@ -0,0 +1,17 @@
+const PORT = process.env.PORT || 8080;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+
+app.use(cors());
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+)
+
+app.listen(PORT, function() {
+    console.log(PORT);
+})
